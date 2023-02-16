c99212785b097b8ac0b5337d696c6aa51fa385cf
JiriPavela <xpavel32@stud.fit.vutbr.cz>
2023-02-16 17:04:30 +0100

    Add CG manager class and (de)serialization of CG.
    The CG manager class is a wrapper over the CG representation
    that implements operations related to CG lookup, storage and
    retrieval using the Stats module.
    
    The CG currently supports only (compresseed) JSON serialization.

--- perun/logic/call_graph/__init__.py
+++ perun/logic/call_graph/__init__.py
@@ -1,77 +1,300 @@
-""" Specification of the new call graph representation.
+"""Implementation of Call Graph (CG) and Control Flow Graph (CFG) representations.
 
-1. Use networkx DiGraph.
-    A. Nodes: function name.
-    B. Node attributes: dict of custom objects {'base', 'level', 'cfg', ?}
-    C. No separate CFG to save space, instead, CFG is an attribute of the CG node.
-2. The call graph class should also contain some identification of the extraction source
-   and additional context information:
-    1. Networkx graph.
-    2. Graph flavours.
-    3. Binary and/or source files used for the extraction (substitute with Project when the Tracer
-       refactoring is done).
-    4. VCS-specific version identification:
-        - When the repository contains no local uncommitted changes, use the VCS hash.
-        - Otherwise, use the ID of the current dirty version (should be supplied by Perun). The ID
-          will possibly contain hash, timestamp and dirty/clean flag. This ID will be used to match
-          the CG to the actual dirty sub-version.
-    5. Call graph version VCS-hash (identifies the extraction source, i.e., files from #2.3):
-        - Can be used to check if we already have a matching call graph stored in the file system.
-            - However, allow to force re-extract a call graph (e.g., in case something changed, such
-              as extraction module / library version, but it is not affecting the hash).
-        - We can't use the VCS version hash here, since it may contain changes to files irrelevant
-          from the CG extraction point of view (e.g., change to a README won't affect the CG).
-        - Use VCS-specific hashing algorithm (e.g., git hash-object).
-        - The collection of files must be sorted!
-    6. Profiling configuration ID.
-        - The optimization settings is irrelevant for a raw and static CG flavours.
-    7. Optimization configuration ID.
-    8. Dirty flag, representing whether the call graph was extracted while at least one file from
-       #2.3 is modified (in any way, including renames) according to the VCS.
-    9. The status (output from the VCS status and from os stat) + hashes of the changed, untracked
-       and not-in-vcs files from #2.3. Store it as an accompanying file to the call graph file,
-       preferably in a compressed format.
-3. We must support multiple flavours of a CG:
-    1. Raw; extracted by <tool>. Not all stored nodes need to be reachable from main.
-        - There can be multiple raw instances, one for each extraction tool.
-    2. Static; built from raw, remove all unreachable edges and nodes.
-    3. Dynamic; built from the current run call graph. CFG must be obtained from the raw CG.
-        - There can be additional dynamic instances for different profiling IDs.
-    4. Mixed; combination of Static and Dynamic CGs to obtain a more general CG.
-    5. Generic (abstract); a special flavour that represents a subsets of other flavours.
-    A. In general, all of the CG flavours must be obtainable in a single Tracer run.
-    B. However, it is not required. Only certain flavours may be extracted if requested by the user
-       or decided by, e.g., the optimization engine.
-    C. The edges and nodes will contain an attribute 'type' with information about the membership
-       of the element into a subset of the flavours. During the extraction or loading, the
-       requested flavour will be obtained as a subgraph.
-    D. Some other node or edge attributes (e.g., level) may be specific to a certain flavour.
-    E. Dynamic and Mixed flavours will be approximations if the run was optimized.
-        - Use the same dynamic CG as in the previous version but remove:
-            a. nodes that refer to deleted functions,
-            b. edges that were removed in the Static CG.
-4. Each CG should have entry points specified. The entry points are used, e.g., in level estimation.
-    A. Executable binaries (optionally with libraries): we can assume that entry point is 'main'.
-    B. Standalone libraries: we must somehow reconstruct the entry points.
-        - Scout the extraction tools.
-5. The call graph must be serializable, ideally compressed json.
-    - Pickle would be the best, but unstable w.r.t. API changes etc.
-    A. It must be possible to combine multiple flavour CG files with compatible configuration into
-       a single file for storage optimization.
-    B. Moreover, the Dynamic and Mixed flavours may be updated with subsequent runs (even with
-       different workload / arguments). The only relevant differentiator is the VCS hash.
-6. The CG file (A) and version status (B) name formats:
-    A. sub_<cg-version-hash>_<c|d>/<profiling-ID>_<r+s+d+m>_<o|f>_<a+...>.cg.bz2
-        - <r+s+d+m> specifies the flavours of the stored call graph.
-            - E.g., 'rd' identifies a generic call graph, containing both raw and dynamic flavours.
-        - <c|d> specifies flags: (clean | dirty)
-        - <o|f> specifies flags: (optimized | full)
-        - <a+...> specifies static CG extraction tools (angr, ...)
-    - example: sub_c9b8cdc7_c/nightly_rd_f_a.cg.bz2
-    B. sub_<cg-version-hash>_<c|d>/<profiling-ID>_<timestamp>.vinfo.bz2
-    - example: sub_c9b8cdc7_c/nightly_2022-04-30-21-22-15.vinfo.bz2
+This package contains everything related to CG and CFG representations used in Perun, namely:
+ - CG-specific Stats file paths;
+ - CG versioning that combines VCS versions and relevant profiling configuration;
+ - CG (CFG) extraction (reconstruction) tools that build the CG / CFG from source or binary files;
+ - layered CG representation, i.e., multiple CG variants stored within a single graph structure;
+ - TODO: a read-only CG view class that supports easy nodes/edges iteration or access;
+ - TODO: CG and CFG comparison algorithms;
+ - easy-to-use operations for file system related manipulations of stored CGs (such as saving,
+   loading, lookup, ...) using the Stats module.
 """
 
+from __future__ import annotations
 
-class CallGraph:
-    pass
+from collections.abc import Sequence, Iterable
+from pathlib import Path
+
+import demandimport
+
+from perun.utils import partition_list
+from perun.collect.identification import CollectCompoundId
+from perun.logic.stats import StatsFile, iter_files, iter_predecessors
+
+from perun.logic.call_graph.structs import ValidStates, VersionState, TIMESTAMP_FMT
+from perun.logic.call_graph.version import CGVersion
+from perun.logic.call_graph.path import CallGraphPath
+from perun.logic.call_graph.graphs import CallGraph, FuncCFG
+from perun.logic.call_graph.extractors import SupportedExtractors, extractor_factory
+
+# Import on demand due to the circular dependency with the io module
+with demandimport.enabled():
+    import perun.logic.call_graph.io as cg_io
+
+
+class CallGraphManager:
+    """A manager class for CG lookup, storage, retrieval and extraction.
+
+    This class wraps the CG representation and provides a range of operations related to CG
+    lookup, storage and retrieval using the Stats module. These operations are fairly complex as
+    CGs are related to both 1) VCS version (i.e., a CG is linked to a specific VCS version),
+    and 2) current state of files the CG depends on (e.g., source or binary files used for
+    extraction).
+
+    The CG versioning is complicated for multiple reasons:
+     A) We want to avoid repeated extraction of a CG when there were no changes.
+     B) We want to be able to detect when a change happened and extract a new version of the CG.
+     C) We want to identify CGs that can be compared across VCS versions, that is, while the VCS
+        version is different, the configuration of CG extraction was the same.
+
+    To solve this problem, the CG is versioned using two IDs:
+     1) A hash value of the files (including their content) used for extraction. The hash value
+        is obtained using the underlying VCS hashing algorithm (e.g., SHA for Git). This hash value
+        changes when there are any changes in the source or binary files used for the CG extraction.
+        Note that there is currently no analysis being done on whether the changes actually impact
+        the resulting CG or not. We call this ID a 'CG version hash'.
+     2) An ID of the extraction/profiling configuration. The ID is either a user-supplied name of
+        the configuration (e.g., 'nightly-build') or a hash value computed from the file paths (!!)
+        used for CG extraction. Since the hash is computed from the paths and not their contents,
+        it is possible to match CGs across project versions despite the files content likely
+        changing. For larger projects with extraction files being added, deleted and renamed, it
+        is advised to name the configuration explicitly. We call this ID a 'configuration name'.
+
+    To help reduce the storage space taken by the stored CGs, the manager keeps only the most recent
+    CG of the same configuration name within a VCS version.
+
+    :ivar call_graph: the actual CG representation.
+    :ivar version: the CG version.
+    """
+
+    def __init__(self, call_graph: CallGraph, cg_version: CGVersion) -> None:
+        """Initializer.
+
+        :ivar call_graph: the call graph representation.
+        :ivar version: the call graph version.
+        """
+        self.call_graph: CallGraph = call_graph
+        self.version: CGVersion = cg_version
+
+    # TODO: compound ID contains optimizations, CG contains them as well. Should we make them
+    #  consistent, or can we remove the optimizations from the compound ID?
+    # TODO: The two 'files' (the parameter and an attribute in compound ID) are a bit confusing.
+    @classmethod
+    def current(
+        cls,
+        compound_id: CollectCompoundId,
+        files: Sequence[Path],
+        extractor: SupportedExtractors = SupportedExtractors.ANGR,
+    ) -> CallGraphManager:
+        """Obtain a call graph for the current version.
+
+        By current version, we mean:
+         1) the VCS version (e.g., rev 8acf5...),
+         2) the CG version hash (i.e., the current state of the files used for extraction),
+         3) and the configuration name.
+
+        If a CG matching the current version is not found, the method attempts to extract the CG.
+
+        :param compound_id: the collection ID containing a valid configuration name.
+        :param files: files used for the CG extraction.
+        :param extractor: the CG extractor to use in case there is no CG for the current version.
+
+        :return: an initialized CG manager containing a current version CG.
+        """
+        # The CG version hash corresponding to the files
+        cg_version_hash, _ = CGVersion.compute_hashes(files)
+        # We do not supply a change state: possibly expensive to compute and one CG version hash
+        # shouldn't have both clean and dirty variant = hashing collision
+        # The VCS version is implicitly HEAD
+        name_pattern = CallGraphPath.lookup_pattern(
+            cg_version_hash=cg_version_hash,
+            conf_name=compound_id.conf_name,
+        )
+        saved_file = cls._select_cg(list(iter_files(name_pattern)))
+        if saved_file:
+            return cls.load(saved_file)
+        # No saved CG file, extract it
+        return cls.extract(compound_id, files, extractor)
+
+    @classmethod
+    def old(
+        cls, compound_id: CollectCompoundId, change_state: ValidStates, minor_version: str
+    ) -> CallGraphManager | None:
+        """Obtain a call graph linked to some previous VCS version.
+
+        The matching CG is searched only in the supplied VCS version. In order to match, the CG
+        must have the same configuration name. The CG version hash is ignored.
+
+        :param compound_id: the collection ID containing a valid configuration name.
+        :param change_state: specifies whether the CG version can originate from a dirty (clean)
+                             repository, or both.
+        :param minor_version: the VCS version to search in.
+
+        :return: an initialized CG manager or None if no such CG was found.
+        """
+        name_pattern = CallGraphPath.lookup_pattern(
+            change_state=change_state, conf_name=compound_id.conf_name, minor_version=minor_version
+        )
+        saved_file = cls._select_cg(list(iter_files(name_pattern)))
+        if saved_file:
+            return cls.load(saved_file)
+        return None
+
+    @classmethod
+    def extract(
+        cls, compound_id: CollectCompoundId, files: Sequence[Path], extractor: SupportedExtractors
+    ) -> CallGraphManager:
+        """Extract a current version CG.
+
+        :param compound_id: the collection ID containing a valid configuration name.
+        :param files: files used for the CG extraction.
+        :param extractor: the CG extractor to use.
+
+        :return: an initialized CG manager with the newly extracted call graph.
+        """
+        # TODO: this is a bit hacky right now.
+        #  Replace files and compound id with some 'Project' object after Tracer refactoring.
+        main_binary, libs = None, None
+        if len(files) >= 1:
+            main_binary = files[0]
+        if len(files) >= 2:
+            libs = files[1:]
+        return CallGraphManager(
+            extractor_factory(extractor, main_binary, libs).extract(),
+            CGVersion.current(compound_id, files),
+        )
+
+    @property
+    def filepath(self) -> CallGraphPath:
+        """Construct a stats file path for the CG.
+
+        Note that the path is provided regardless if the CG is saved or not.
+
+        :return: a stats file path corresponding to the CG.
+        """
+        return CallGraphPath(
+            self.version.cg_version_hash,
+            self.version.change_state.value,
+            self.version.cid.conf_name,
+            self.version.version_timestamp.strftime(TIMESTAMP_FMT),
+            self.version.vcs_version,
+        )
+
+    def predecessor(self) -> StatsFile[CallGraphPath] | None:
+        """Obtain a previous version of the CG (in terms of VCS versions), if it exists.
+
+        This method traverses the VCS history and attempts to find the most recent previous VCS
+        version that contains a matching CG.
+
+        :return: a stats file with the predecessor CG, or None if there is no such predecessor.
+        """
+        # Only the configuration name and minor version are relevant for the predecessors lookup
+        name_pattern = CallGraphPath.lookup_pattern(
+            conf_name=self.version.cid.conf_name, minor_version=self.version.vcs_version
+        )
+        # Obtain only call graphs from the previous minor version
+        for previous_cgs in iter_predecessors(name_pattern, top=1):
+            return self._select_cg([StatsFile(cg) for cg in previous_cgs])
+        return None
+
+    @classmethod
+    def load(cls, file: CallGraphPath | StatsFile[CallGraphPath]) -> CallGraphManager:
+        """Load a CG manager specified by a path or a stats file.
+
+        :param file: the CG file (path).
+
+        :return: an initialized CG manager, or None if the file could not be loaded.
+        """
+        return cg_io.load(file)
+
+    def save(self) -> CallGraphPath:
+        """Save the current call graph.
+
+        :return: a path to the saved file.
+        """
+        # Recalculate every existing layer before saving the CG
+        self.call_graph.recalculate()
+        cg_file = cg_io.save(self)
+        # Make sure that we do not cause inconsistency = multiple redundant files
+        # Find and delete CG files that store old version of the same CG
+        redundant_pattern = CallGraphPath.lookup_pattern(
+            change_state=self.version.change_state.value,
+            conf_name=self.version.cid.conf_name,
+            minor_version=self.filepath.minor_version,
+        )
+        for other_cg in iter_files(redundant_pattern):
+            if other_cg != cg_file:
+                other_cg.delete()
+        return cg_file.filepath
+
+    @staticmethod
+    def _select_cg(cgs: Sequence[StatsFile[CallGraphPath]]) -> StatsFile[CallGraphPath] | None:
+        """Select the most suitable CG file among multiple candidate files in the same VCS version.
+
+        One VCS version can contain multiple CG files. Most notably, different configuration IDs
+        (names) can have their own separate CG versions. However, there can also occasionally be
+        multiple CG files for a single configuration name, such as one or more CG files with
+        different CG version hash or different version state (i.e., clean or dirty working dir).
+        Any such files, except the most recent one per each version state, are considered redundant.
+
+        This method expects that the provided CG files are within the same VCS version and have the
+        same configuration name. As such, the files should only differ in the CG version hash and/or
+        the version state. Hence, there are at most two files that should be kept and the remaining
+        files are considered redundant, and will be deleted.
+
+        When both clean and dirty versions of a CG exist, this method prefers the clean version.
+
+        :param cgs: multiple CG files with the same VCS version and configuration name.
+
+        :return: the most suitable CG file among the provided collection of CGs.
+        """
+        if not cgs:
+            return None
+        if len(cgs) == 1:
+            return cgs[0]
+        # Multiple CGs, remove potential old versions
+        cgs = CallGraphManager._remove_old(cgs)
+        # At most one clean and one dirty version should exist.
+        clean, dirty = partition_list(
+            cgs, lambda v: v.filepath.change_state == VersionState.CLEAN.value
+        )
+        # Prefer a clean version
+        if clean:
+            return clean[0]
+        if dirty:
+            return dirty[0]
+        return None
+
+    @staticmethod
+    def _remove_old(versions: Iterable[StatsFile[CallGraphPath]]) -> list[StatsFile[CallGraphPath]]:
+        """Delete redundant versions of call graphs stored within the Perun storage.
+
+        This method identifies which stored CG files are no longer needed and deletes them.
+        There can be at most one CG file per (VCS version, configuration name, version state) tuple.
+        When selecting what file to keep, the most recent one is selected and the remaining ones are
+        deleted.
+
+        :param versions: a collection of CG files.
+
+        :return: filtered collection of CG files that contains no redundant file.
+        """
+        # First partition the files per 1) VCS version, 2) configuration name and 3) version state
+        partition: dict[tuple[str, str, str], list[StatsFile[CallGraphPath]]] = {}
+        for file in versions:
+            f_path = file.filepath
+            if not file.exist():
+                continue
+            assert f_path.minor_version is not None  # Existing files have a valid minor version
+            partition.setdefault(
+                (f_path.minor_version, f_path.conf_name, f_path.change_state), []
+            ).append(file)
+        # Delete any old redundant files
+        for files in partition.values():
+            if len(files) <= 1:
+                continue
+            # From the most recent date to the latest one
+            files.sort(key=lambda f: f.filepath.timestamp, reverse=True)
+            for file in files[1:]:
+                file.delete()
+        return sum(partition.values(), [])
---
+++ perun/logic/call_graph/io.py
@@ -0,0 +1,194 @@
+"""This module implements serialization and deserialization of CG and CFG.
+
+While the serialization and deserialization of CG, CFG, and any nested objects could be solved by
+implementing (de)serialization methods within the classes directly, this would likely clutter the
+class design with additional responsibility unrelated to its primary function.
+
+We also deliberately separated the (de)serialization implementation from the __init__ file,
+even though this created a circular dependency between the files (the circular dependency is
+solved through lazy module importing). This was done to reduce the namespace pollution that would
+manifest when importing directly from the call_graph package through its __init__ file.
+
+Currently, the module implements only JSON (de)serialization. However, the resulting JSON is then
+additionally compressed by the stats module.
+"""
+
+from __future__ import annotations
+from typing import Any
+
+import json
+from enum import Enum
+from pathlib import Path
+from datetime import datetime
+
+import networkx as nx
+
+from perun.utils.structs import VCSChangeState
+from perun.collect.identification import CollectCompoundId
+from perun.logic.stats import StatsFile
+
+from perun.logic.call_graph import CallGraphManager
+from perun.logic.call_graph.graphs import CallGraph, FuncCFG
+from perun.logic.call_graph.path import CallGraphPath
+from perun.logic.call_graph.version import FileChangeDetail, CGVersion
+from perun.logic.call_graph.structs import (
+    CGLayer,
+    CGElementLayers,
+    CGFlavour,
+    VersionState,
+    TIMESTAMP_FMT,
+)
+
+
+def save(cg_manager: CallGraphManager) -> StatsFile:
+    """Save the CG manager to a stats file.
+
+    :param cg_manager: the CG manager object.
+
+    :return: the stored stats file reference.
+    """
+    version_stats_file = StatsFile(cg_manager.filepath)
+    with version_stats_file.open("wt") as cg_handle:
+        json.dump(cg_manager, cg_handle, cls=CGJsonEncoder)
+    return version_stats_file
+
+
+def load(file: CallGraphPath | StatsFile[CallGraphPath]) -> CallGraphManager:
+    """Load a CG manager from the provided path or file.
+
+    :param file: the CG file specification.
+
+    :return: the loaded CG manager.
+    """
+    if isinstance(file, CallGraphPath):
+        file = StatsFile(file)
+    with file.open("rt") as cg_handle:
+        return json.load(cg_handle, cls=CGJsonDecoder)
+
+
+class CGJsonEncoder(json.JSONEncoder):
+    """Custom JSON encoder class for encoding the CG classes."""
+
+    def default(self, o: Any) -> Any:
+        """Handle serialization of custom objects.
+
+        For custom CG classes, we include the '_t' key into the serialized
+        dictionary to recognize the class when performing deserialization.
+
+        :param o: the object to serialize.
+
+        :return: JSON-serialized object.
+        """
+        # pylint: disable=too-many-return-statements
+        if isinstance(o, Enum):
+            # Works only for enums that have a string as a value
+            return o.value
+        if isinstance(o, Path):
+            return str(o)
+        if isinstance(o, datetime):
+            return o.strftime(TIMESTAMP_FMT)
+        if isinstance(o, CollectCompoundId):
+            return {"f": list(o.files), "c": o.conf_name, "o": list(o.opt_ids), "!t": "cid"}
+        if isinstance(o, FileChangeDetail):
+            return {
+                "f": o.file,
+                "h": o.file_hash,
+                "s": o.state,
+                "d": o.state_detail,
+                "n": o.new_file,
+                "fs": o.file_size,
+                "m": o.modify_t,
+                "!t": "fchd",
+            }
+        if isinstance(o, CGVersion):
+            return {
+                "c": o.cid,
+                "v": o.vcs_version,
+                "h": o.cg_version_hash,
+                "s": {str(src): hsh for src, hsh in o.sources.items()},
+                "t": o.version_timestamp,
+                "cs": o.change_state,
+                "ch": {str(file): detail for file, detail in o.changes.items()},
+                "!t": "cgv",
+            }
+        if isinstance(o, CGLayer):
+            return {
+                "f": o.flavour.value if o.flavour is not None else "",
+                "o": o.optimization if o.optimization is not None else "",
+                "!t": "cgl",
+            }
+        if isinstance(o, CallGraph):
+            return {
+                "g": nx.adjacency_data(o.graph),
+                "s": o.entry.static,
+                "d": dict(o.entry.dynamic),
+                "!t": "cg",
+            }
+        if isinstance(o, FuncCFG):
+            return {"g": nx.adjacency_data(o.graph), "!t": "cfg"}
+        if isinstance(o, CGElementLayers):
+            return {"l": list(o.layers), "!t": "cge"}
+        if isinstance(o, CallGraphManager):
+            return {"c": o.call_graph, "v": o.version, "!t": "cgm"}
+        return super().default(o)
+
+
+class CGJsonDecoder(json.JSONDecoder):
+    """Custom JSON decoder class for decoding custom CG classes."""
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        """Initializer.
+
+        Registers the custom object decoding method.
+        """
+        super().__init__(object_hook=self.obj_hook, *args, **kwargs)
+
+    @staticmethod
+    def obj_hook(obj: Any) -> Any:
+        """Implement deserialization for CG classes.
+
+        The method uses the '_t' key to determine a) if the JSON element is actually a custom CG
+        object, and 2) the resulting class to construct using the element.
+
+        :param obj: the object to deserialize.
+
+        :return: either the deserialized object or the original JSON primitive.
+        """
+        # pylint: disable=too-many-return-statements
+        if "!t" not in obj:
+            return obj
+        if obj["!t"] == "cid":
+            return CollectCompoundId(obj["c"], {Path(file) for file in obj["f"]}, set(obj["o"]))
+        if obj["!t"] == "fchd":
+            return FileChangeDetail(
+                Path(obj["f"]),
+                obj["h"],
+                VCSChangeState(obj["s"]),
+                obj.get("sd", ""),
+                obj.get("n", ""),
+                int(obj["fs"]),
+                datetime.strptime(obj["m"], TIMESTAMP_FMT),
+            )
+        if obj["!t"] == "cgv":
+            return CGVersion(
+                obj["c"],
+                obj["v"],
+                obj["h"],
+                {Path(src): hsh for src, hsh in obj["s"].items()},
+                datetime.strptime(obj["t"], TIMESTAMP_FMT),
+                VersionState(obj["cs"]),
+                {Path(file): detail for file, detail in obj["ch"].items()},
+            )
+        if obj["!t"] == "cgl":
+            return CGLayer(
+                CGFlavour(obj["f"] if obj["f"] else None), obj["o"] if obj["o"] else None
+            )
+        if obj["!t"] == "cg":
+            return CallGraph(nx.adjacency_graph(obj["g"]), obj["s"], obj["d"])
+        if obj["!t"] == "cfg":
+            return FuncCFG(nx.adjacency_graph(obj["g"]))
+        if obj["!t"] == "cge":
+            return CGElementLayers(*obj["l"])
+        if obj["!t"] == "cgm":
+            return CallGraphManager(obj["c"], obj["v"])
+        return obj
